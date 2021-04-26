80de7eb3d465cf70f282769755c7f51b7257b10f
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-26 15:12:46 -0400

    Update Product.jsx

--- src/Product.jsx
+++ src/Product.jsx
@@ -3,7 +3,8 @@ import React from "react";
 function Product(props) {
     return (
       <div>
-        <h2>{ props.product.name }</h2>
+        <h2>{props.product.name}</h2>
+        <p>{props.product.price.toLocalString("en-US", {style: "currency", currency: "USD"})}</p>
       </div>
     );
 }
